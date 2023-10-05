70712b2cfc49109dc39928d33e3f6fb67a0c8a5c
Nathanael Vieira <vieira_trabalho@hotmail.com>
2023-10-05 10:43:01 -0300

    BackgroundArea: criando area de fundo com seu estilo simples

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,41 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+	private static Stage PRIMARY_STAGE;
+	private static Scene SCENE = null;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			PRIMARY_STAGE = primaryStage;
+			PRIMARY_STAGE.initStyle(StageStyle.TRANSPARENT);
+
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/MainView.fxml"));
+
+			Scene scene = new Scene(root, 551, 275, Color.rgb(0, 0, 0, 0));
+
+			PRIMARY_STAGE.setScene(scene);
+			PRIMARY_STAGE.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public static Stage getPRIMARY_STAGE() {
+		return PRIMARY_STAGE;
+	}
+
 }
---
+++ src/gui/MainVIewController.java
@@ -0,0 +1,24 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+
+public class MainVIewController implements Initializable {
+
+	@FXML
+	private Group backgroundArea;
+
+	@FXML
+	private Rectangle rectangledArea;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
